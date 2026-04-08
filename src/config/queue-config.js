68bbbcf1f846ed7f8e10464033515382a52a9bf9
kishansinghifs1 <kishansingh956196@gmail.com>
2026-04-08 17:33:04 +0530

    Refactor database connection and queue configuration to use environment variables

--- src/config/queue-config.js
+++ src/config/queue-config.js
@@ -1,20 +1,25 @@
 const amqplib=require("amqplib");
+const ServerConfig = require('./server-config');
 let channel,connection;
 async function connectQueue(){
     try {
-         connection =await amqplib.connect("amqp://localhost");
+         connection =await amqplib.connect(ServerConfig.RABBITMQ_URL);
          channel =await connection.createChannel();
 
-       await channel.assertQueue("noti-queue");
+       await channel.assertQueue(ServerConfig.RABBITMQ_QUEUE);
     } catch (error) {
        console.log(error) ;
+       throw error;
     }
 }
 async function sendData(data){
     try {
-        await channel.sendToQueue("noti-queue",Buffer.from(JSON.stringify(data)));
+        if (!channel) {
+            return;
+        }
+        await channel.sendToQueue(ServerConfig.RABBITMQ_QUEUE,Buffer.from(JSON.stringify(data)));
     } catch (error) {
-        
+        console.log(error);
     }
 }
 module.exports={
--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 module.exports={
     PORT: process.env.PORT,
-    FLIGHT_SERVICE:process.env.FLIGHT_SERVICE
+    FLIGHT_SERVICE:process.env.FLIGHT_SERVICE,
+    RABBITMQ_URL: process.env.RABBITMQ_URL || 'amqp://rabbitmq:5672',
+    RABBITMQ_QUEUE: process.env.RABBITMQ_QUEUE || 'noti-queue'
 }
